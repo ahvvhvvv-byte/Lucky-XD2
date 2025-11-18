db274443c9077ce7e3f4e277fb6fc8402b428069
ahvvhvvv-byte <ahvvhvvv@gmail.com>
2025-11-19 01:46:04 +0200

    Delete settingss.js

--- settingss.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-  SESSION_ID: "",  // add your Session Id here
-  OWNER_NUMBER: "", // put your phone number here
-  PREFIX: ".", // prefix (e.g., ., /, !, *)
-  TIMEZONE: "Africa/Kampala" //put your country timeZone....leave blank if u don't know.
-};
